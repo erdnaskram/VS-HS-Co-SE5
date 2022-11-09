b4b8196f4e576658caa0dc3bac3a53947563599c
Zoe Schulz <zoe.schulz@mail.de>
2022-11-09 14:01:31 +0100

    Blatt 3 angefangen

--- Praktikum/src/Blatt3/RMIServer.java
+++ Praktikum/src/Blatt3/RMIServer.java
@@ -1,11 +1,29 @@
 package Blatt3; //rmilernen.tokenfibon
 
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
 public class RMIServer implements RMIInterface{
 
     private static String servername;
+    private long longIncrement = 0;
 
     public static void main(String[] args) {
         servername = args[0];
+        RMIServer server = new RMIServer();
+
+        try {
+            RMIInterface stub = (RMIInterface) UnicastRemoteObject.exportObject(server, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("RMIInterface", stub);
+        } catch (AlreadyBoundException | RemoteException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -37,24 +55,34 @@ public class RMIServer implements RMIInterface{
     @Override
     public String[] rmiSplitAt(String s, int i) {
         String[] response = new String[2];
-        response[0] = s.substring(0, i);
-        response[1] = s.substring(i, s.length());
 
-        return new String[0];
+        if(i<0){
+            response[0] = "";
+            response[1] = s;
+        }else if(i >=s.length()){
+            response[0] = s;
+            response[1] = "";
+        }else {
+            response[0] = s.substring(0, i);
+            response[1] = s.substring(i, s.length());
+        }
+        return response;
     }
 
     @Override
     public void rmiIncrement() {
-
+        longIncrement++;
     }
 
     @Override
     public void rmiShutdown() {
-
+        //TODO
     }
 
     @Override
     public char rmiServernameAtWithException(int i) throws Exception {
-        return 0;
+        if(i >= servername.length() || i <0)
+            throw new Exception();
+        return servername.charAt(i);
     }
 }
